aa1dda0001a75f75644eecbeeef4a578baaadb14
aploftus <aploftus@users.noreply.github.com>
2017-11-10 15:52:03 -0800

    Mock up table header for project data
    Add example data array

--- client/components/project-list.js
+++ client/components/project-list.js
@@ -2,15 +2,45 @@ angular.module('donorsApp')
 
 .component('projectList', {
   controller: function() {
-    this.school = {
-      name: 'Hack Reactor'
-    };
+    this.projects = [
+      {
+        title: 'title1',
+        city: 'city1',
+        state: 'state1',
+        costToComplete: 'costToComplete1',
+        expirationDate: 'expirationDate1'
+      },
+      {
+        title: 'title2',
+        city: 'city2',
+        state: 'state2',
+        costToComplete: 'costToComplete2',
+        expirationDate: 'expirationDate2'
+      },
+      {
+        title: 'title3',
+        city: 'city3',
+        state: 'state3',
+        costToComplete: 'costToComplete3',
+        expirationDate: 'expirationDate3'
+      }
+    ];
   },
 
   template:
     '<div>' +
       '<h2>Projects List Component</h2>' +
-      '<p>School: {{ $ctrl.school.name }}</p>' +
+      // '<p>School: {{ $ctrl.school.name }}</p>' +
+      '<div class="table">' +
+        '<div class="table-head">' +
+          '<span class="cell header">Project Name</span>' +
+          '<span class="cell header">City</span>' +
+          '<span class="cell header">State</span>' +
+          '<span class="cell header">Amount Left</span>' +
+          '<span class="cell header">Expiration Date</span>' +
+        '</div>' +
+        '<project-list-entry ng-repeat="project in $ctrl.projects" project={{project}}></project-list-entry>' +
+      '</div>' +
     '</div>',
 
 });
---
+++ client/exampleData.js
@@ -0,0 +1,285 @@
+window.exampleData = [
+  {
+    "id": "2663967",
+    "hashCode": "-800518293",
+    "proposalURL": "https://www.donorschoose.org/project/wobble-chairs-for-wiggly-kindergarteners/2663967/?verify=-800518293&utm_source=api&utm_medium=feed&utm_content=bodylink&utm_campaign=DONORSCHOOSE",
+    "fundURL": "https://secure.donorschoose.org/donors/givingCart.html?proposalid=2663967&donationAmount=45&verify=-800518293&utm_source=api&utm_medium=feed&utm_content=fundlink&utm_campaign=DONORSCHOOSE",
+    "imageURL": "https://storage.donorschoose.net/dc_prod/images/user/small/u4536818_sm.jpg?timestamp=1499785930083",
+    "retinaImageURL": "https://storage.donorschoose.net/dc_prod/images/user/retina/u4536818_retina.jpg?timestamp=1499785930083",
+    "thumbImageURL": "https://storage.donorschoose.net/dc_prod/images/user/thumbnail/u4536818_thmb.jpg?timestamp=1499785930083",
+    "title": "Wobble Chairs for Wiggly Kindergarteners!",
+    "shortDescription": "My students come to school energetic and ready to learn every day! They are passionate about learning and thrive in an environment that fosters creativity, mobility and comfort. They enjoy being...",
+    "fulfillmentTrailer": "My students need comfortable flexible seating options that provide safe mobility while staying on task.",
+    "snippets": [],
+    "percentFunded": "84",
+    "numDonors": "11",
+    "costToComplete": "88.28",
+    "studentLed": false,
+    "numStudents": "20",
+    "professionalDevelopment": false,
+    "matchingFund": {
+      "matchingKey": "",
+      "ownerRegion": "",
+      "name": "",
+      "donorSalutation": "",
+      "type": "",
+      "matchImpactMultiple": "",
+      "multipleForDisplay": "",
+      "logoURL": "",
+      "faqURL": "",
+      "amount": "0.00",
+      "description": ""
+    },
+    "totalPrice": "548.28",
+    "freeShipping": "true",
+    "teacherId": "4536818",
+    "teacherName": "Ms. Weber",
+    "gradeLevel": {
+      "id": "1",
+      "name": "Grades PreK-2"
+    },
+    "povertyLevel": "",
+    "povertyType": {
+      "label": "LOW",
+      "name": "",
+      "range": "0%-30%",
+      "showPovertyLevel": "false"
+    },
+    "teacherTypes": [],
+    "schoolTypes": [{
+      "id": 12,
+      "name": "Horace Mann (partner)"
+    }],
+    "schoolName": "Bain Elementary School",
+    "schoolUrl": "https://www.donorschoose.org/school/bain-elementary-school/1572/",
+    "city": "Charlotte",
+    "zip": "28227-7552",
+    "state": "NC",
+    "stateFullName": "North Carolina",
+    "latitude": "35.175320506095886",
+    "longitude": "-80.647867396473885",
+    "zone": {
+      "id": "301",
+      "name": "North Carolina"
+    },
+    "subject": {
+      "id": "8",
+      "name": "Mathematics",
+      "groupId": "4"
+    },
+    "additionalSubjects": [{
+      "id": "10",
+      "name": "Literacy",
+      "groupId": "6"
+    }],
+    "resource": {
+      "id": "3",
+      "name": "Supplies"
+    },
+    "expirationDate": "2017-11-28",
+    "fundingStatus": "needs funding"
+  },
+  {
+    "id": "2680674",
+    "hashCode": "338519214",
+    "proposalURL": "https://www.donorschoose.org/project/cool-fun-and-comfortable-seating/2680674/?verify=338519214&utm_source=api&utm_medium=feed&utm_content=bodylink&utm_campaign=DONORSCHOOSE",
+    "fundURL": "https://secure.donorschoose.org/donors/givingCart.html?proposalid=2680674&donationAmount=45&verify=338519214&utm_source=api&utm_medium=feed&utm_content=fundlink&utm_campaign=DONORSCHOOSE",
+    "imageURL": "https://storage.donorschoose.net/dc_prod/images/user/small/u4611198_sm.jpg?timestamp=1501914661556",
+    "retinaImageURL": "https://storage.donorschoose.net/dc_prod/images/user/retina/u4611198_retina.jpg?timestamp=1501914661556",
+    "thumbImageURL": "https://storage.donorschoose.net/dc_prod/images/user/thumbnail/u4611198_thmb.jpg?timestamp=1501914661556",
+    "title": "Cool, Fun, and Comfortable Seating",
+    "shortDescription": "I work in a Title I school teaching first graders. The students I serve live in a high poverty area. They receive free breakfast and lunch. Most of them come from a single parent home or have...",
+    "fulfillmentTrailer": "My students need flexible seating to collaborate with each other and maximize their learning potential.",
+    "snippets": [],
+    "percentFunded": "64",
+    "numDonors": "13",
+    "costToComplete": "306.91",
+    "studentLed": false,
+    "numStudents": "25",
+    "professionalDevelopment": false,
+    "matchingFund": {
+      "matchingKey": "",
+      "ownerRegion": "",
+      "name": "",
+      "donorSalutation": "",
+      "type": "",
+      "matchImpactMultiple": "",
+      "multipleForDisplay": "",
+      "logoURL": "",
+      "faqURL": "",
+      "amount": "0.00",
+      "description": ""
+    },
+    "totalPrice": "846.49",
+    "freeShipping": "true",
+    "teacherId": "4611198",
+    "teacherName": "Mrs. Smith",
+    "gradeLevel": {
+      "id": "1",
+      "name": "Grades PreK-2"
+    },
+    "povertyLevel": "More than three‑quarters of students from low‑income households",
+    "povertyType": {
+      "label": "HIGH",
+      "name": "More than three‑quarters of students from low‑income households",
+      "range": "76-89%",
+      "showPovertyLevel": "true"
+    },
+    "teacherTypes": [],
+    "schoolTypes": [
+      {
+        "id": 12,
+        "name": "Horace Mann (partner)"
+      },
+      {
+        "id": 14,
+        "name": "Kia (partner)"
+      },
+      {
+        "id": 15,
+        "name": "Kia (partner)"
+      },
+      {
+        "id": 3,
+        "name": "Special Education"
+      },
+      {
+        "id": 17,
+        "name": "Kia (partner)"
+      },
+      {
+        "id": 4,
+        "name": "Magnet"
+      }
+    ],
+    "schoolName": "Hornets Nest Elementary School",
+    "schoolUrl": "https://www.donorschoose.org/school/hornets-nest-elementary-school/1615/",
+    "city": "Charlotte",
+    "zip": "28216-1910",
+    "state": "NC",
+    "stateFullName": "North Carolina",
+    "latitude": "35.324309000000000",
+    "longitude": "-80.866262000000000",
+    "zone": {
+      "id": "301",
+      "name": "North Carolina"
+    },
+    "subject": {
+      "id": "8",
+      "name": "Mathematics",
+      "groupId": "4"
+    },
+    "additionalSubjects": [{
+      "id": "3",
+      "name": "Literature &amp; Writing",
+      "groupId": "6"
+    }],
+    "resource": {
+      "id": "3",
+      "name": "Supplies"
+    },
+    "expirationDate": "2017-12-04",
+    "fundingStatus": "needs funding"
+  },
+  {
+    "id": "2899594",
+    "hashCode": "-737694048",
+    "proposalURL": "https://www.donorschoose.org/project/privacy-please/2899594/?verify=-737694048&utm_source=api&utm_medium=feed&utm_content=bodylink&utm_campaign=DONORSCHOOSE",
+    "fundURL": "https://secure.donorschoose.org/donors/givingCart.html?proposalid=2899594&donationAmount=45&verify=-737694048&utm_source=api&utm_medium=feed&utm_content=fundlink&utm_campaign=DONORSCHOOSE",
+    "imageURL": "https://storage.donorschoose.net/dc_prod/images/user/small/u564212_sm.jpg?timestamp=1502578848458",
+    "retinaImageURL": "https://storage.donorschoose.net/dc_prod/images/user/retina/u564212_retina.jpg?timestamp=1502578848458",
+    "thumbImageURL": "https://storage.donorschoose.net/dc_prod/images/user/thumbnail/u564212_thmb.jpg?timestamp=1502578848458",
+    "title": "Privacy Please!",
+    "shortDescription": "YES, our school is considered a Title I school and many of the children live in low income housing, hotels, and yes, some are even in shelters. But that does not define them. <br />These amazing...",
+    "fulfillmentTrailer": "My students need a classroom mailbox and privacy partitions for organization and assessment accuracy!",
+    "snippets": [],
+    "percentFunded": "71",
+    "numDonors": "2",
+    "costToComplete": "62.98",
+    "studentLed": false,
+    "numStudents": "25",
+    "professionalDevelopment": false,
+    "matchingFund": {
+      "matchingKey": "",
+      "ownerRegion": "",
+      "name": "",
+      "donorSalutation": "",
+      "type": "",
+      "matchImpactMultiple": "",
+      "multipleForDisplay": "",
+      "logoURL": "",
+      "faqURL": "",
+      "amount": "0.00",
+      "description": ""
+    },
+    "totalPrice": "215.92",
+    "freeShipping": "true",
+    "teacherId": "564212",
+    "teacherName": "Mrs. Johnston",
+    "gradeLevel": {
+      "id": "1",
+      "name": "Grades PreK-2"
+    },
+    "povertyLevel": "Nearly all students from low‑income households",
+    "povertyType": {
+      "label": "HIGHEST",
+      "name": "Nearly all students from low‑income households",
+      "range": "90%+",
+      "showPovertyLevel": "true"
+    },
+    "teacherTypes": [{
+      "id": 5,
+      "name": "SONIC Teacher"
+    }],
+    "schoolTypes": [
+      {
+        "id": 12,
+        "name": "Horace Mann (partner)"
+      },
+      {
+        "id": 14,
+        "name": "Kia (partner)"
+      },
+      {
+        "id": 15,
+        "name": "Kia (partner)"
+      },
+      {
+        "id": 3,
+        "name": "Special Education"
+      },
+      {
+        "id": 17,
+        "name": "Kia (partner)"
+      }
+    ],
+    "schoolName": "Highland Renaissance Academy",
+    "schoolUrl": "https://www.donorschoose.org/school/highland-renaissance-academy/1613/",
+    "city": "Charlotte",
+    "zip": "28206-2029",
+    "state": "NC",
+    "stateFullName": "North Carolina",
+    "latitude": "35.256069000000000",
+    "longitude": "-80.803032000000000",
+    "zone": {
+      "id": "301",
+      "name": "North Carolina"
+    },
+    "subject": {
+      "id": "26",
+      "name": "Character Education",
+      "groupId": "5"
+    },
+    "additionalSubjects": [{
+      "id": "18",
+      "name": "Early Development",
+      "groupId": "5"
+    }],
+    "resource": {
+      "id": "13",
+      "name": "Reading Nooks, Desks &amp; Storage"
+    },
+    "expirationDate": "2018-03-05",
+    "fundingStatus": "needs funding"
+  }
+];
