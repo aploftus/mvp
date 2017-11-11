c59f5f4da7ec556484d35af2ba61f6d137d5cc58
aploftus <aploftus@users.noreply.github.com>
2017-11-10 16:21:12 -0800

    Update styling, confirm project-list operating

--- client/components/project-list.js
+++ client/components/project-list.js
@@ -2,6 +2,7 @@ angular.module('donorsApp')
 
 .component('projectList', {
   controller: function() {
+    
     this.projects = [
       {
         title: 'title1',
@@ -31,15 +32,8 @@ angular.module('donorsApp')
     '<div>' +
       '<h2>Projects List Component</h2>' +
       '<div>Total projects: {{$ctrl.projects.length}}</div>' +
-      '<div class="table">' +
-        '<div class="table-head">' +
-          '<span class="cell header">Project Name</span>' +
-          '<span class="cell header">City</span>' +
-          '<span class="cell header">State</span>' +
-          '<span class="cell header">Amount Left</span>' +
-          '<span class="cell header">Expiration Date</span>' +
-        '</div>' +
-        '<project-list-entry ng-repeat="project in $ctrl.projects" project={{project}}></project-list-entry>' +
+      '<div class="project-list">' +
+        '<project-list-entry ng-repeat="project in $ctrl.projects" project="project"></project-list-entry>' +
       '</div>' +
     '</div>',
 
